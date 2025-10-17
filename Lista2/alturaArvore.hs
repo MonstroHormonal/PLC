b76f789828f96c23c56bfc7d7639c06d80ed0b37
Álvaro <145587864+MonstroHormonal@users.noreply.github.com>
2025-10-17 10:37:51 -0300

    Add files via upload

---
+++ Lista2/alturaArvore.hs
@@ -0,0 +1,11 @@
+data Tree t = Node t (Tree t) (Tree t) | Nilt
+              deriving (Read)
+
+alturaArvore :: Tree t -> Int
+alturaArvore Nilt = 0
+alturaArvore (Node t t1 t2) = max (alturaArvore t1) (alturaArvore t2) + 1
+
+main = do
+       a <- getLine
+       let result = alturaArvore (read a::Tree Int)
+       print result
